211f7d211a4747561971aee97685b93b24c32bf2
BRBergman <blake.bergman@gmail.com>
2024-09-30 14:35:11 -0400

    added 404 page

--- src/main.rs
+++ src/main.rs
@@ -7,33 +7,36 @@ fn main() {
     let server = Server::http(ip).unwrap();
 
     for request in server.incoming_requests() {
-        println!(
+        /*println!(
             "Request\n url: {:?}\n, headers: {:?}",
             //request.method(),
             request.url(),
             request.headers()
-        );
-
+        );*/
         let path_served = Path::new(request.url().split_once('/').unwrap().1);
         let x = Path::new("../web/").join(path_served);
 
         //make that i just give the name and it adds .html or something
-        let serve_file: Response<File>;
+        let serve_file;
         if x.exists() {
             match path_served.extension() {
                 Some(_) => {
                     serve_file = Response::from_file(File::open(x).unwrap());
                 }
                 None => {
-                    let pathx =
-                        File::open(Path::new("../web/").join(path_served).join("index.html"))
-                            .unwrap();
-                    serve_file = Response::from_file(pathx);
+                    match File::open(Path::new("../web/").join(path_served).join("index.html")) {
+                        Ok(x) => serve_file = Response::from_file(x),
+                        Err(_) => {
+                            println!("i dont know what brings you here");
+                            serve_file = Response::from_file(
+                                File::open(Path::new("../web/404.html")).unwrap(),
+                            )
+                        }
+                    }
                 }
             };
         } else {
-            let index_file = File::open(Path::new("../web/index.html")).unwrap();
-            serve_file = Response::from_file(index_file);
+            serve_file = Response::from_file(File::open(Path::new("../web/404.html")).unwrap());
         }
         let _ = request.respond(serve_file);
     }
