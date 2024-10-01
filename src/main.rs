dd056dc30493ba87bdf8062c28c197b32259d2a4
BRBergman <blake.bergman@gmail.com>
2024-10-01 14:19:46 -0400

    put resultpath enum in a different file

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@ use std::path::PathBuf;
 use std::{fs::File, path::Path};
 use tiny_http::{Response, Server};
 
+mod result_path;
+use result_path::{ResultPath, ToResultPath};
+
 fn main() {
     let server = Server::http(SocketAddrV4::new(Ipv4Addr::UNSPECIFIED, 8000)).unwrap();
     println!("running");
@@ -33,40 +36,5 @@ fn main() {
                 let _ = request.respond(file_404_page);
             }
         };
-    }
-}
-
-impl ToResultPath for Path {
-    fn to_result_path(&self) -> ResultPath {
-        ResultPath::from_path(self.to_path_buf())
-    }
-}
-impl ToResultPath for PathBuf {
-    fn to_result_path(&self) -> ResultPath {
-        ResultPath::from_path(self.to_path_buf())
-    }
-}
-trait ToResultPath {
-    fn to_result_path(&self) -> ResultPath;
-}
-enum ResultPath {
-    File(PathBuf),
-    Directory(PathBuf),
-    Err(Error),
-}
-
-impl ResultPath {
-    fn from_path(path: PathBuf) -> ResultPath {
-        if path.exists() {
-            if path.is_file() {
-                return ResultPath::File(path);
-            } else if path.is_dir() {
-                return ResultPath::Directory(path);
-            }
-        }
-        return ResultPath::Err(Error::new(
-            std::io::ErrorKind::NotFound,
-            path.to_str().unwrap(),
-        ));
     }
 }
---
+++ src/result_path.rs
@@ -0,0 +1,38 @@
+use std::path::PathBuf;
+use std::io::Error;
+use std::path::Path;
+
+impl ToResultPath for Path {
+    fn to_result_path(&self) -> ResultPath {
+        ResultPath::from_path(self.to_path_buf())
+    }
+}
+impl ToResultPath for PathBuf {
+    fn to_result_path(&self) -> ResultPath {
+        ResultPath::from_path(self.to_path_buf())
+    }
+}
+pub(crate) trait ToResultPath {
+    fn to_result_path(&self) -> ResultPath;
+}
+pub enum ResultPath {
+    File(PathBuf),
+    Directory(PathBuf),
+    Err(Error),
+}
+
+impl ResultPath {
+    fn from_path(path: PathBuf) -> ResultPath {
+        if path.exists() {
+            if path.is_file() {
+                return ResultPath::File(path);
+            } else if path.is_dir() {
+                return ResultPath::Directory(path);
+            }
+        }
+        return ResultPath::Err(Error::new(
+            std::io::ErrorKind::NotFound,
+            path.to_str().unwrap(),
+        ));
+    }
+}
