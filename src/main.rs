b86aba822730e2e35f79747c2e0de4c6d3a2fb7b
BRBergman <blake.bergman@gmail.com>
2024-10-01 09:58:09 -0400

    made more clean

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,22 @@ use std::{fs::File, path::Path};
 use tiny_http::{Response, Server};
 
 fn main() {
-    let ip = SocketAddrV4::new(Ipv4Addr::UNSPECIFIED, 8000);
-    let server = Server::http(ip).unwrap();
+    let server = Server::http(SocketAddrV4::new(Ipv4Addr::UNSPECIFIED, 8000)).unwrap();
 
     for request in server.incoming_requests() {
+        let file_404_page = Response::from_string("404 :(");
         let path_served = Path::new(request.url().split_once('/').unwrap().1);
         let folder_default = Path::new("../web/");
-        let x = folder_default.join(path_served);
+        let path_full = folder_default.join(path_served);
         //make so that it adds the .htm so i dont have to
         //make that i just give the name and it adds .html or something
-        let file_404_page = Response::from_string("404 :(");
-        if x.exists() {
+        if path_full.exists() {
             match path_served.extension() {
                 Some(_) => {
-                    let _ = request.respond(Response::from_file(File::open(x).unwrap()));
+                    let _ = request.respond(Response::from_file(File::open(path_full).unwrap()));
                 }
                 None => {
-                    match File::open(folder_default.join(path_served).join("index.html")) {
+                    match File::open(path_full.join("index.html")) {
                         Ok(x) => {
                             let _ = request.respond(Response::from_file(x));
                         }
