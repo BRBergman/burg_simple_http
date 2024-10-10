3fd5e2ff75abdeb7c6cc0cf7fb01932024859793
BRBergman <blake.bergman@gmail.com>
2024-10-10 13:50:10 -0400

    idk

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,14 @@ use result_path::{ResultPath, ToFile, ToResultPath};
 fn main() {
     let server = Server::http(SocketAddrV4::new(Ipv4Addr::UNSPECIFIED, 8000)).unwrap();
     println!("running");
+    // test
     for request in server.incoming_requests() {
         let file_404_page = "404 :(".to_response();
         let path_full = std::env::current_dir()
             .unwrap()
             .join(Path::new("web"))
             .join(Path::new(request.url().trim_start_matches('/')));
-        println!("{:?}", path_full);
+        println!("{}", path_full.display());
         match path_full.to_result_path() {
             ResultPath::File(file) => {
                 let _ = request.respond(file.to_response());
@@ -38,18 +39,18 @@ fn main() {
     }
 }
 
-trait ToResponseFile{
+trait ToResponseFile {
     fn to_response(self) -> Response<File>;
 }
-impl ToResponseFile for File{
+impl ToResponseFile for File {
     fn to_response(self) -> Response<File> {
-       Response::from_file(self)
+        Response::from_file(self)
     }
 }
-trait ToResponseStr{
+trait ToResponseStr {
     fn to_response(&self) -> Response<std::io::Cursor<Vec<u8>>>;
 }
-impl ToResponseStr for str{
+impl ToResponseStr for str {
     fn to_response(&self) -> Response<std::io::Cursor<Vec<u8>>> {
         Response::from_string(self)
     }
