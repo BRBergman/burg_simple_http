0c5535f40ccfe481fffb7a83528ea27916dcc087
BRBergman <blake.bergman@gmail.com>
2024-10-25 09:41:37 -0400

    css work

--- src/web/home.rs
+++ src/web/home.rs
@@ -1,15 +1,20 @@
+use std::time::SystemTime;
+
 use maud::{html, DOCTYPE};
 
 pub fn home() -> String {
     html! {
+        
             (DOCTYPE)
             html{
             head{
+                link rel="stylesheet" href="style.css";
                 title{"home"}
             }
             body{
                 h1{"Home Page!"}
-                p1{"guh"}
+                p1{"The time is: "((SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_nanos()))}
+                div{p1{"guh"}}
                 div{"hi"}
             
             }
