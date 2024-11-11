1fe13f68e484bdbf171b445caaf58d3de2b27ca9
BRBergman <blake.bergman@gmail.com>
2024-11-11 15:06:14 -0500

    moved toresponce to web.rs

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,19 @@
-use std::io::Cursor;
+
 use std::net::{Ipv4Addr, SocketAddrV4};
 use std::path::PathBuf;
-use tiny_http::{Response, Server};
-use web::Pages;
+use tiny_http::Server;
 mod web;
+use web::ToResponse;
 //https://doc.rust-lang.org/std/keyword.break.html
 //this is really cool^
 fn main() {
-    server();
+    http_server();
 }
-fn server() {
+fn http_server() {
     let server = Server::http(SocketAddrV4::new(Ipv4Addr::UNSPECIFIED, 8000)).unwrap();
-    //let path_base = std::env::current_dir().unwrap().join("website");
     server.incoming_requests().into_iter().for_each(|x| {
-        let url = x.url().to_owned();
-        x.respond(
-            //path_base
-            PathBuf::new()
-                .join(url.trim_start_matches('/'))
-                .to_response(),
-        )
-        .unwrap()
+        let url = PathBuf::from(x.url().trim_start_matches('/'));
+        println!("{}", url.display());
+        x.respond(url.to_response()).unwrap()
     });
-}
-trait ToResponse {
-    fn to_response(self) -> Response<Cursor<Vec<u8>>>;
-}
-impl ToResponse for PathBuf {
-    fn to_response(self) -> Response<Cursor<Vec<u8>>> {
-        let env = std::env::current_dir().unwrap().join("website");
-        Response::from_data(match std::fs::read(env.join(&self)) {
-            Ok(x) => x,
-            Err(_) => match std::fs::read(env.join(&self).join("index.html")) {
-                Ok(x) => x,
-                Err(_) => Pages::default().get_page(self).into_string().into(),
-            },
-        })
-    }
 }
--- src/web.rs
+++ src/web.rs
@@ -1,7 +1,8 @@
 use std::{path::PathBuf, vec};
-
 use home::home;
 use maud::{html, PreEscaped};
+use std::io::Cursor;
+use tiny_http::Response;
 
 pub mod css;
 pub mod home;
@@ -25,7 +26,6 @@ impl Page {
 }
 impl Pages {
     pub fn get_page(&self, path: PathBuf) -> PreEscaped<String> {
-        println!("{}",path.display());
         for page in &self.pages {
             if page.path == path {
                 return page.page.clone();
@@ -34,10 +34,25 @@ impl Pages {
         return not_found();
     }
 }
-impl Default for Pages{
+impl Default for Pages {
     fn default() -> Self {
         Pages {
             pages: vec![Page::new(PathBuf::from("home"), home())],
         }
+    }
+}
+pub trait ToResponse {
+    fn to_response(self) -> Response<Cursor<Vec<u8>>>;
+}
+impl ToResponse for PathBuf {
+    fn to_response(self) -> Response<Cursor<Vec<u8>>> {
+        let env = std::env::current_dir().unwrap().join("website");
+        Response::from_data(match std::fs::read(env.join(&self)) {
+            Ok(x) => x,
+            Err(_) => match std::fs::read(env.join(&self).join("index.html")) {
+                Ok(x) => x,
+                Err(_) => Pages::default().get_page(self).into_string().into(),
+            },
+        })
     }
 }
