b9f73772b64c438c16d240145da4ac317e6ed799
BRBergman <blake.bergman@gmail.com>
2025-02-04 11:23:42 -0500

    moved spawns so now the whole thin is in the spawn

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::{net::{Ipv4Addr, SocketAddrV4}, thread::spawn};
+use std::{
+    net::{Ipv4Addr, SocketAddrV4},
+    thread::spawn,
+};
 use tiny_http::Server;
 mod web;
 use web::DestructedURL;
@@ -13,13 +16,15 @@ fn main() {
     };
     let mut spawns = Vec::new();
     for x in server.incoming_requests().into_iter() {
-        let url = DestructedURL::new(x.url());
-        println!("{}", url);
-        if let Some(_x @ "end") = url.extra_data.as_deref() {
-            server.unblock();
-            break;
-        }
-        spawns.push(spawn(move || x.respond(url.to_web_response())));
+        spawns.push({
+            let url = DestructedURL::new(x.url());
+            println!("{}", url);
+            if let Some(_x @ "end") = url.extra_data.as_deref() {
+                server.unblock();
+                break;
+            }
+            spawn(move || x.respond(url.to_web_response()))
+        });
     }
     for spawn in spawns {
         println!("joining: {:?}", spawn.thread().id());
